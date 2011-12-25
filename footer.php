3217173806376a5779f3bae3313a0f53ddf91e4f
Thomas Scholz <info@toscho.de>
2011-12-25 11:20:19 +0100

    Create Footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php # -*- coding: utf-8 -*-
+/**
+ * @package    Fefe
+ */
+?><div align=right><?php
+! dynamic_sidebar( 'footer' ) and print 'Proudly made with PHP, MySQL and WordPress';
+?></div>
+<?php
+wp_footer();
