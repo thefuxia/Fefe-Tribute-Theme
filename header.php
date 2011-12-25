b90a4b3f66a07f03b168f6027396334c9e056745
Thomas Scholz <info@toscho.de>
2011-12-25 12:08:24 +0100

    Add stylesheet

--- header.php
+++ header.php
@@ -12,6 +12,7 @@ $current_title = wp_title( '|', FALSE );
 print empty ( $current_title ) ? get_bloginfo( 'name' ) : $current_title;
 ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel=stylesheet href="<?php echo get_stylesheet_uri(); ?>">
 <?php
 // Stylesheet is inserted per wp_enqueue_style().
 wp_head();
