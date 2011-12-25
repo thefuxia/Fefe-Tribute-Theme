3aeb350082bd9958f1527c58473bdb1adb3bc8c0
Thomas Scholz <info@toscho.de>
2011-12-25 12:38:18 +0100

    Add searchform shortcode

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 add_action( 'after_setup_theme', 'fefe_setup' );
 add_filter( 'the_content', 'fefe_strip_first_p', 11, 1 );
+add_shortcode( 'searchform', 'fefe_searchform' );
 
 function fefe_setup()
 {
@@ -32,7 +33,7 @@ function fefe_searchform()
 	$query = esc_attr( apply_filters( 'the_search_query', get_search_query( false ) ) );
 	$url = home_url( '/' );
 	$label = __( 'Suchbegriff:', 'fefe' );
-	return "<form method=GET action='$url'><label>$label<input name=s value='$query'><input type=submit></label></form>";
+	return "<form method=GET action='$url'><label>$label <input name=s value='$query'><input type=submit></label></form>";
 }
 
 
