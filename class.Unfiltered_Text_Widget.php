4bd3fd62acff1dd9e3b3edc0fc90ba4736d5ebd3
Thomas Scholz <info@toscho.de>
2011-12-25 12:35:50 +0100

    Add unfiltered text widget

---
+++ class.Unfiltered_Text_Widget.php
@@ -0,0 +1,78 @@
+<?php # -*- coding: utf-8 -*-
+
+/**
+ * Simplified variant of the native text widget class.
+ *
+ * @author Thomas Scholz aka toscho http://toscho.de
+ * @version 1.0
+ */
+class Unfiltered_Text_Widget extends WP_Widget
+{
+	/**
+	 * @uses apply_filters( 'magic_widgets_name' )
+	 */
+	public function __construct()
+	{
+		parent::__construct(
+			'unfiltered_text'
+		,	__( 'Unfiltered Text', 'fefe' )
+		,	array( 'description' => __( 'Pure Markup', 'fefe' ) )
+		,	array( 'width' => 300, 'height' => 150 )
+		);
+	}
+
+	/**
+	 * Output.
+	 *
+	 * @param  array $args
+	 * @param  array $instance
+	 * @return void
+	 */
+	public function widget( $args, $instance )
+	{
+		echo $instance['text'];
+	}
+
+	/**
+	 * Prepares the content. Not.
+	 *
+	 * @param  array $new_instance New content
+	 * @param  array $old_instance Old content
+	 * @return array New content
+	 */
+	public function update( $new_instance, $old_instance )
+	{
+		return $new_instance;
+	}
+
+	/**
+	 * Backend form.
+	 *
+	 * @param array $instance
+	 * @return void
+	 */
+	public function form( $instance )
+	{
+		$instance = wp_parse_args( (array) $instance, array( 'text' => '' ) );
+		$text     = esc_textarea( $instance['text'] );
+?>
+		<textarea class="widefat" rows="7" cols="20" id="<?php
+			echo $this->get_field_id( 'text' );
+		?>" name="<?php
+			echo $this->get_field_name( 'text' );
+		?>"><?php
+			echo $text;
+		?></textarea>
+<?php
+	}
+
+	/**
+	 * Registers this widget.
+	 *
+	 * @return void
+	 */
+	public static function register()
+	{
+		register_widget( __CLASS__ );
+	}
+}
