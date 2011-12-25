d7e9d4b3eb8977d270db95ccb343d12da69dada5
Thomas Scholz <info@toscho.de>
2011-12-25 12:35:23 +0100

    Fixing footer widget. The name 'footer' made WordPress go crazy.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
  * @package    Fefe
  */
 ?><div align=right><?php
-! dynamic_sidebar( 'footer' ) and print 'Proudly made with PHP, MySQL and WordPress';
+! dynamic_sidebar( 'fefe-footer' ) and print 'Proudly made with PHP, MySQL and WordPress';
 ?></div>
 <?php
 wp_footer();
--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@ function fefe_setup()
 {
 	add_theme_support( 'automatic-feed-links' );
 	add_action( 'widgets_init', 'fefe_widgets_setup' );
+	locate_template( array ( 'class.Unfiltered_Text_Widget.php' ), TRUE, TRUE );
+	add_action( 'widgets_init', array ( 'Unfiltered_Text_Widget', 'register' ), 20 );
 }
 
 /**
@@ -25,6 +27,14 @@ function fefe_strip_first_p( $content )
 	return substr( $content, 3, strlen( $content ) );
 }
 
+function fefe_searchform()
+{
+	$query = esc_attr( apply_filters( 'the_search_query', get_search_query( false ) ) );
+	$url = home_url( '/' );
+	$label = __( 'Suchbegriff:', 'fefe' );
+	return "<form method=GET action='$url'><label>$label<input name=s value='$query'><input type=submit></label></form>";
+}
+
 
 /**
  * Registers the sidebars.
@@ -35,24 +45,25 @@ function fefe_widgets_setup()
 {
 	register_sidebar(
 		array (
-			'name'          => __( 'Header-Untertitel', 'fefe' )
-		,	'id'            => 'sub-head'
-		,	'description'   => __( 'Nutze hierzu das Widget »Nur Markup«', 'fefe' )
+			'name'          => __( 'Fusszeile', 'fefe' )
+		// You cannot name it just 'footer' or WordPress breaks in widgets.php.
+		,	'id'            => 'fefe-footer'
+		,	'description'   => __( 'Nutze hierzu das Widget Unfiltered Text', 'fefe' )
 		,	'before_widget' => ''
 		,	'after_widget'  => ''
 		,	'before_title'  => ''
-		,	'after_title'   => '',
+		,	'after_title'   => ''
 		)
 	);
 	register_sidebar(
 		array (
-			'name'          => __( 'Fußzeile', 'fefe' )
-		,	'id'            => 'footer'
-		,	'description'   => __( 'Nutze hierzu das Widget »Nur Markup«', 'fefe' )
+			'name'          => __( 'Header-Untertitel', 'fefe' )
+		,	'id'            => 'sub-head'
+		,	'description'   => __( 'Nutze hierzu das Widget Unfiltered Text', 'fefe' )
 		,	'before_widget' => ''
 		,	'after_widget'  => ''
 		,	'before_title'  => ''
-		,	'after_title'   => '',
+		,	'after_title'   => ''
 		)
 	);
 }
