ccd5da411223dc6b45a0d5b661f25e59904b915b
Thomas Scholz <info@toscho.de>
2011-12-26 10:03:18 +0100

    formatting, simplified code

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ get_header();
 
 if ( ! is_page() )
 {
-	print '<h2><a href="' . home_url( '/' ) . '" style="text-decoration:none;color:black">' . get_bloginfo( 'name' ) . '</a></h2>';
-	print '<b>' . get_bloginfo( 'description' ) . '</b>';
+	print '<h2><a href="' . home_url( '/' )
+		. '" style="text-decoration:none;color:black">'
+		. get_bloginfo( 'name' ) . '</a></h2>'
+		. '<b>' . get_bloginfo( 'description' ) . '</b>';
 
-	if ( is_active_sidebar( 'head' ) )
-	{
-		print '<p align=right>';
-		dynamic_sidebar( 'head' );
-	}
+	is_active_sidebar( 'head' )
+		and print '<p align=right>'
+		and dynamic_sidebar( 'head' );
 }
 else
 {
@@ -32,16 +32,13 @@ if ( is_archive() or is_front_page() or is_search() or is_single() )
 	while ( have_posts() )
 	{
 		the_post();
+
 		$ftt_date_new = get_the_date( 'D M d Y' );
-		if ( $ftt_date != $ftt_date_new )
-		{
-			print "</ul><h3>$ftt_date_new</h3><ul>";
-		}
+		$ftt_date != $ftt_date_new and print "</ul><h3>$ftt_date_new</h3><ul>";
 		$ftt_date = $ftt_date_new;
 
 		print '<li><a href="' . get_permalink() . '">[1]</a> ';
 		the_content();
-		#the_excerpt();
 		wp_link_pages();
 	}
 
